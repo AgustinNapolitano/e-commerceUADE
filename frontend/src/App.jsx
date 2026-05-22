9f733a50c0f9610e2c0ec99f3975f3d356079cfb
Lucho235 <142629253+Lucho235@users.noreply.github.com>
2026-05-22 18:02:14 -0300

    Revert "feat: implementar seccion dinamica de categorias"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import ProductList from './components/ProductList'
-import Navbar from './components/Navbar'
-import CategoryList from './components/CategoryList'
 import NavBar from './components/NavBar'
 import ProductDetail from './components/ProductDetail'
 import {Routes, Route} from 'react-router-dom'
@@ -15,7 +13,6 @@ return (
       <Routes>
         <Route path="/" element={<h1>Inicio</h1>} />
         <Route path="/productos" element={<ProductList />} />
-        <Route path="/categorias" element={<CategoryList />} />
         <Route path="/productos/:id" element={<ProductDetail />} />
         <Route path="/categorias" element={<h1>Categorías</h1>} />
         <Route path="/pedidos" element={<h1>Pedidos</h1>} />
--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './ProductList.css';
 
 const ProductList = () => {
-  const location = useLocation();
   const [products, setProducts] = useState([]);
   const [textoBusqueda, setTextoBusqueda] = useState("");
-  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(
-    location.state?.category || "Todas"
-  );
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("Todas");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
