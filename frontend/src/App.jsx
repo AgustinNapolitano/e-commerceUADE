e8eed700d7d07c98f6af2c4885f13e5cf02aaa73
Agustin Napolitano <agustinnapo@gmail.com>
2026-05-22 16:52:55 -0300

    Agregamos NavBar y configuramos las rutas iniciales utilizando React Router.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import ProductList from './components/ProductList';
+import ProductList from './components/ProductList'
+import Navbar from './components/Navbar'
+import {Routes, Route} from 'react-router-dom'
+
+
 function App() {
 
-  return (
+return (
     <>
-      <ProductList />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<h1>Inicio</h1>} />
+        <Route path="/productos" element={<ProductList />} />
+        <Route path="/categorias" element={<h1>Categorías</h1>} />
+        <Route path="/pedidos" element={<h1>Pedidos</h1>} />
+      </Routes>
     </>
-  );
+  )
 }
 
 export default App;
