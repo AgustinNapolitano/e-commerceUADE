e64fc111212cba0f70296a868dfbe2c104e4a904
Lucho235 <llamas@uade.edu.ar>
2026-05-23 14:02:12 -0300

    Clean up duplicated imports and routes in App.jsx and ProductList.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import ProductList from './components/ProductList'
-import Navbar from './components/Navbar'
 import CategoryList from './components/CategoryList'
 import NavBar from './components/NavBar'
 import ProductDetail from './components/ProductDetail'
@@ -15,9 +14,8 @@ return (
       <Routes>
         <Route path="/" element={<h1>Inicio</h1>} />
         <Route path="/productos" element={<ProductList />} />
-        <Route path="/categorias" element={<CategoryList />} />
         <Route path="/productos/:id" element={<ProductDetail />} />
-        <Route path="/categorias" element={<h1>Categorías</h1>} />
+        <Route path="/categorias" element={<CategoryList />} />
         <Route path="/pedidos" element={<h1>Pedidos</h1>} />
       </Routes>
     </>
